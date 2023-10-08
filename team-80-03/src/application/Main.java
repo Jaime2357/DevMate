0bd06d54023853d8c6e97b1aa7841dd13c1d093c
ggmaddr <ggiangta@gmail.com>
2023-10-08 06:06:30 -0700

    commit

--- team-80-03/src/application/Main.java
+++ team-80-03/src/application/Main.java
@@ -10,28 +10,25 @@ import javafx.fxml.FXMLLoader;
  * This class is for setting the scene and running the DevMate application.
  */
 public class Main extends Application {
-	
 	@Override
-	public void start(Stage primaryStage) {
-		
+	public void start(Stage primaryStage) {	
+		//local comment test
 		try {
 			HBox mainBox = (HBox)FXMLLoader.load(getClass().getClassLoader().getResource("view/Main.fxml"));
 			Scene scene = new Scene(mainBox);
 			scene.getStylesheets().add(getClass().getClassLoader().getResource("css/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
 			// set reference of Hbox mainBox in the commonObjs object
 			CommonObjs commonObjs = CommonObjs.getInstance();
 			commonObjs.setMainBox(mainBox);
-			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
 	// Launch DevMate application
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 }
